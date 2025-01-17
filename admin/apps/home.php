fb9d84502ff04be42b65ad5381d9a5bb8e038335
marc_go <goringm64@gmail.com>
2025-01-17 20:24:27 +0100

    Update home.php
    Add check

--- admin/apps/home.php
+++ admin/apps/home.php
@@ -3,6 +3,7 @@ ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 
+require $_SERVER["DOCUMENT_ROOT"] . "/admin/bin/check.php";
 require $_SERVER["DOCUMENT_ROOT"] . "/admin/bin/admin.php";
 
 $session = new loginManager();
