60d946801852b67d84d1c3b1d29ad7bf05b8e034
marc_go <goringm64@gmail.com>
2025-02-22 15:31:46 +0100

    Delete admin directory

--- admin/bin/check.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!file_exists($_SERVER["DOCUMENT_ROOT"] . "/config.php")) {
-	header("Location: /setup.php");
-	exit;
-}
-?>
--- admin/bin/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require $_SERVER["DOCUMENT_ROOT"] . "/config.php";
-
-$conn = new mysqli($db_host, $db_user, $db_pw, $db_name);
-?>
