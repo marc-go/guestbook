3f199ca99d44067a8934aa2c37ad55359963cb55
marc_go <goringm64@gmail.com>
2025-02-23 18:29:32 +0100

    Update index.php
    Added footer

--- index.php
+++ index.php
@@ -20,6 +20,37 @@ require $_SERVER["DOCUMENT_ROOT"] . "/admin/bin/check.php";
     </header>
     <main class="mdl-layout__content">
       <div class="page-content" style="padding: 20px;">
+  		<dialog class="mdl-dialog">
+    	  <h4 class="mdl-dialog__title">Suggest a new feature</h4>
+    	  <div class="mdl-dialog__content">
+      		<p>
+        	  Thank you that you want to suggest a new feature.
+      		</p>
+			<form action="/bin/suggestion.php" method="post">
+			  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" name="feature" id="feature" required>
+                <label class="mdl-textfield__label" for="feature">Feature</label>
+              </div>
+              <div class="mdl-dialog__actions">
+      		    <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">Send</button>
+      			<button type="button" class="mdl-button close" onclick="dialog.close()">Close</button>
+    	  	  </div>
+			</form>
+		  </div>
+  		</dialog>
+  		<script>
+    	  var dialog = document.querySelector('dialog');
+    	  var showDialogButton = document.querySelector('#show-dialog');
+    	  if (! dialog.showModal) {
+      		dialogPolyfill.registerDialog(dialog);
+    	  }
+    	  showDialogButton.addEventListener('click', function() {
+      	    dialog.showModal();
+    	  });
+    	  dialog.querySelector('.close').addEventListener('click', function() {
+      		dialog.close();
+    	  });
+  		</script>
         <h1>Guestbook</h1>
 		<p>
 			<?php
@@ -86,6 +117,17 @@ require $_SERVER["DOCUMENT_ROOT"] . "/admin/bin/check.php";
         </div>
       </div>
     </main>
+	<footer class="mdl-mini-footer">
+  	  <div class="mdl-mini-footer__left-section">
+        <div class="mdl-logo">marc-go/guestbook</div>
+  	  </div>
+  	  <div class="mdl-mini-footer__right-section">
+    	<ul class="mdl-mini-footer__link-list">
+      	  <li><a href="https://github.com/marc-go/guestbook.git">GitHub</a></li>
+      	  <li><a onclick="dialog.showModal()">Suggest a new feature</a></li>
+        </ul>
+  	  </div>
+	</footer>
   </div>
   <script src="/js/remove.js"></script>
 </body>
